e84cee7fac05aacc3cffca793117711bd2a84638
Duane Bekaert <duane.bekaert@gmail.com>
2014-07-11 03:05:09 +0200

    Enhanced signal handling for the edition page. We're now tackling with the very last functionalities.

--- include/MainWindow.h
+++ include/MainWindow.h
@@ -6,6 +6,7 @@
 #include <gtkmm/stack.h>
 #include <gtkmm/stackswitcher.h>
 #include <gtkmm/box.h>
+#include <gtkmm/bin.h>
 #include <gtkmm/button.h>
 #include <gtkmm/buttonbox.h>
 #include <gtkmm/label.h>
@@ -35,6 +36,9 @@ namespace dosman {
             void on_quit();
             void on_row_selected();
 
+            void on_save_edited_entry();
+            void on_run_edited_entry();
+
             void update_collection_widget();
             void update_edition_widget();
 
@@ -51,14 +55,20 @@ namespace dosman {
                                             m_edition_box,
                                             m_edition_entry_box;
             Gtk::HPaned                     m_edition_panned;
-            Gtk::ButtonBox                  m_edit_buttons_box;
+            Gtk::ButtonBox                  m_edit_buttons_box,
+                                            m_edit_save_run_box;
             Gtk::StackSwitcher              m_stack_switcher;
             Gtk::Stack                      m_stack;
             Gtk::Button                     m_edit_add,
-                                            m_edit_remove;
-            Gtk::ScrolledWindow             m_scrolled_window;
+                                            m_edit_remove,
+                                            m_edit_save,
+                                            m_edit_run;
+            Gtk::ScrolledWindow             m_scrolled_window,
+                                            m_edit_entry;
             Gtk::FlowBox                    m_flow_box;
-            Gtk::Image                      m_default_image;
+
+            /* content of the edition wndow */
+            Gtk::Label                      m_entry_title;
 
             std::map<std::string, Gtk::Button*> m_buttons;
             std::map<std::string, Gtk::Image*>  m_images;
--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -11,9 +11,12 @@ dosman::MainWindow::MainWindow() :
     m_edition_box(Gtk::ORIENTATION_VERTICAL),
     m_edition_entry_box(Gtk::ORIENTATION_VERTICAL),
     m_edit_buttons_box(Gtk::ORIENTATION_HORIZONTAL),
+    m_edit_save_run_box(Gtk::ORIENTATION_HORIZONTAL),
     m_entries_num(0),
     m_edit_add(Gtk::Stock::ADD),
-    m_edit_remove(Gtk::Stock::REMOVE)
+    m_edit_remove(Gtk::Stock::REMOVE),
+    m_edit_save("Save"),
+    m_edit_run("Run")
 {
     set_title("DOSMAN");
     set_border_width(10);
@@ -26,7 +29,7 @@ dosman::MainWindow::MainWindow() :
 
     m_entry_manager = EntryManager::Initialize();
 
-    // Collection page
+    /* Collection page */
     m_flow_box.set_homogeneous(true);
     m_flow_box.set_selection_mode(Gtk::SELECTION_NONE);
     m_flow_box.set_resize_mode(Gtk::RESIZE_PARENT);
@@ -39,15 +42,13 @@ dosman::MainWindow::MainWindow() :
 
     update_collection_widget();
 
-    // Edition page
+    /* end of Collection page */
+
+    /* Edition page */
     m_reftreemodel = Gtk::ListStore::create(m_columns);
     m_treeview.set_model(m_reftreemodel);
     m_treeview.append_column("Name", m_columns.m_col_name);
 
-    /*
-    m_treeview.signal_row_activated().connect( sigc::mem_fun(*this,
-              &MainWindow::on_row_selected) );
-    */
     m_treeview.signal_cursor_changed().connect(sigc::mem_fun(*this,
               &MainWindow::on_row_selected) );
 
@@ -58,6 +59,10 @@ dosman::MainWindow::MainWindow() :
             &MainWindow::on_add_new_entry));
     m_edit_remove.signal_clicked().connect( sigc::mem_fun(*this, 
             &MainWindow::on_remove_selected_entry));
+    m_edit_run.signal_clicked().connect( sigc::mem_fun(*this, 
+            &MainWindow::on_run_edited_entry));
+    m_edit_save.signal_clicked().connect( sigc::mem_fun(*this, 
+            &MainWindow::on_save_edited_entry));
 
     m_edit_buttons_box.set_layout(Gtk::BUTTONBOX_EXPAND);
 
@@ -67,13 +72,25 @@ dosman::MainWindow::MainWindow() :
     m_edition_panned.pack1(m_edition_box, Gtk::SHRINK);
     m_edition_panned.add2(m_edition_entry_box);
 
+    m_edit_save_run_box.pack_end(m_edit_run, Gtk::PACK_SHRINK, 10);
+    m_edit_save_run_box.pack_end(m_edit_save, Gtk::PACK_SHRINK, 10);
+
+    m_edit_save_run_box.set_layout(Gtk::BUTTONBOX_END);
+
+    m_edition_entry_box.pack_start(m_edit_entry, Gtk::PACK_EXPAND_WIDGET);
+    m_edition_entry_box.pack_end(m_edit_save_run_box, Gtk::PACK_SHRINK);
+
+    m_edit_entry.add(m_entry_title);
+
     update_edition_widget();
+
+    /* end of edition page */
 
     m_stack.set_transition_type(Gtk::STACK_TRANSITION_TYPE_SLIDE_LEFT_RIGHT);
     m_stack.set_transition_duration(500);
 
     m_stack.add(m_collection_box, "m_collection_foo", "Collection"); 
-    m_stack.add(m_edition_panned, "m_edition_bar", "Edition"); // SHOULD BE USING A PANED CONTAINER
+    m_stack.add(m_edition_panned, "m_edition_bar", "Edition");
 
     m_stack_switcher.set_stack(m_stack);
 
@@ -118,7 +135,31 @@ void dosman::MainWindow::on_quit() {
 
 void dosman::MainWindow::on_row_selected()
 {
-    std::cout << "top lel" << std::endl;
+    Glib::RefPtr<Gtk::TreeSelection> selection = m_treeview.get_selection();
+    Gtk::TreeModel::iterator selectedRow = selection->get_selected();
+    Gtk::TreeModel::Row row = *selectedRow;
+    Glib::ustring port = row.get_value(m_columns.m_col_name);
+
+    std::string tmp_key(port.data());
+    if (tmp_key.compare("") == 0)  return;
+    m_entry_title.set_text(tmp_key);
+}
+
+void dosman::MainWindow::on_run_edited_entry()
+{
+    Glib::RefPtr<Gtk::TreeSelection> selection = m_treeview.get_selection();
+    Gtk::TreeModel::iterator selectedRow = selection->get_selected();
+    Gtk::TreeModel::Row row = *selectedRow;
+    Glib::ustring port = row.get_value(m_columns.m_col_name);
+
+    std::string tmp_key(port.data());
+    if (tmp_key.compare("") == 0)  return;
+    m_entry_manager->getEntryMap()->at(tmp_key.c_str()).run();
+}
+
+void dosman::MainWindow::on_save_edited_entry()
+{
+    std::cout << "Let's save" << std::endl;
 }
 
 void dosman::MainWindow::update_collection_widget() {
